d1d8ec2763ffd64d92b4e12d270ca830c305491c
Jared Hanson <jaredhanson@gmail.com>
2012-12-27 12:14:04 -0800

    Add additional test cases from Node.

--- test/querystring.test.js
+++ test/querystring.test.js
@@ -78,6 +78,12 @@ function(querystring) {
           expect(testCase[2]).to.deep.equal(querystring.parse(testCase[0]));
         });
       });
+      
+      it('should stringify as expected', function() {
+        qsTestCases.forEach(function(testCase) {
+          expect(testCase[1]).to.equal(querystring.stringify(testCase[2]));
+        });
+      });
     });
     
     describe('colon tests cases', function() {
@@ -96,6 +102,72 @@ function(querystring) {
         // test that the colon test cases can do the same
         qsColonTestCases.forEach(function(testCase) {
           expect(testCase[2]).to.deep.equal(querystring.parse(testCase[0], ';', ':'));
+        });
+      });
+      
+      it('should stringify as expected', function() {
+        qsColonTestCases.forEach(function(testCase) {
+          expect(testCase[1]).to.equal(querystring.stringify(testCase[2], ';', ':'));
+        });
+      });
+    });
+    
+    describe('weird objects', function() {
+      // [wonkyObj, qs, canonicalObj]
+      var extendedFunction = function() {};
+      extendedFunction.prototype = {a: 'b'};
+      var qsWeirdObjects = [
+        [{regexp: /./g}, 'regexp=', {'regexp': ''}],
+        [{regexp: new RegExp('.', 'g')}, 'regexp=', {'regexp': ''}],
+        [{fn: function() {}}, 'fn=', {'fn': ''}],
+        [{fn: new Function('')}, 'fn=', {'fn': ''}],
+        [{math: Math}, 'math=', {'math': ''}],
+        [{e: extendedFunction}, 'e=', {'e': ''}],
+        [{d: new Date()}, 'd=', {'d': ''}],
+        [{d: Date}, 'd=', {'d': ''}],
+        [{f: new Boolean(false), t: new Boolean(true)}, 'f=&t=', {'f': '', 't': ''}],
+        [{f: false, t: true}, 'f=false&t=true', {'f': 'false', 't': 'true'}],
+        [{n: null}, 'n=', {'n': ''}],
+        [{nan: NaN}, 'nan=', {'nan': ''}],
+        [{inf: Infinity}, 'inf=', {'inf': ''}]
+      ];
+      // }}}
+      
+      it('should parse as expected', function() {
+        // test the weird objects, that they get parsed properly
+        qsWeirdObjects.forEach(function(testCase) {
+          expect(testCase[2]).to.deep.equal(querystring.parse(testCase[1]));
+        });
+      });
+      
+      it('should stringify as expected', function() {
+        qsWeirdObjects.forEach(function(testCase) {
+          expect(testCase[1]).to.equal(querystring.stringify(testCase[0]));
+        });
+      });
+    });
+    
+    describe('parse id', function() {
+      it('should parse as expected', function() {
+        expect('918854443121279438895193').to.equal(querystring.parse('id=918854443121279438895193').id);
+      });
+    });
+    
+    describe('no munge', function() {
+      var qsNoMungeTestCases = [
+        ['', {}],
+        ['foo=bar&foo=baz', {'foo': ['bar', 'baz']}],
+        /* ['foo=bar&foo=baz', foreignObject], */
+        ['blah=burp', {'blah': 'burp'}],
+        ['gragh=1&gragh=3&goo=2', {'gragh': ['1', '3'], 'goo': '2'}],
+        ['frappucino=muffin&goat%5B%5D=scone&pond=moose',
+         {'frappucino': 'muffin', 'goat[]': 'scone', 'pond': 'moose'}],
+        ['trololol=yes&lololo=no', {'trololol': 'yes', 'lololo': 'no'}]
+      ];
+      
+      it('should parse as expected', function() {
+        qsNoMungeTestCases.forEach(function(testCase) {
+          expect(testCase[0]).to.deep.equal(querystring.stringify(testCase[1], '&', '=', false));
         });
       });
     });
